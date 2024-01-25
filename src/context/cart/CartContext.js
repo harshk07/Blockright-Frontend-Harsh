7a3d7f7c91e00aff7b757cefe166c52f7fb40979
HarshK07 <khapreharsh07@gmail.com>
2024-01-25 12:23:10 +0530

    Changes to be committed: 	modified:   src/context/cart/CartContext.js

--- src/context/cart/CartContext.js
+++ src/context/cart/CartContext.js
@@ -9,6 +9,11 @@ const cartReducer = (state, action) => {
     switch (action.type) {
         case 'ADD_TO_CART':
             return { ...state, items: [...state.items, action.payload] };
+        case 'REMOVE_FROM_CART':
+            return {
+                ...state,
+                items: state.items.filter((_, index) => index !== action.payload),
+            };
         default:
             return state;
     }
