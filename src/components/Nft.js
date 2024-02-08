2922f7073f94902f1c026e98b00baaa8bf010dc0
Krishna <krishnacool781@gmail.com>
2024-02-08 21:34:31 +0530

    fixed nftid

--- src/components/Nft.js
+++ src/components/Nft.js
@@ -1,27 +1,43 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import WalletIDMainContext from "../context/walletID/WalletIDMainContext";
 import { useContext } from "react";
 
-export const Nft = ({ imgSource, name, price }) => {
+export const Nft = ({ imgSource, name, nftId }) => {
   const navigate = useNavigate();
-  const { setNftName, setFetchNftImage } = useContext(WalletIDMainContext);
+  const { setNftName, setFetchNftImage, setNftId } =
+    useContext(WalletIDMainContext);
 
-  function handleSubmit(imgSource, name, price) {
-    const data = { "imgSource": imgSource, "name": name, "price": price };
+  function handleSubmit(imgSource, name, nftId) {
+    const data = { imgSource: imgSource, name: name, nftId: nftId };
+    console.log(data);
     setNftName(name);
     setFetchNftImage(imgSource);
-    navigate('/Rightspage', { state: { data } });
+    setNftId(nftId);
+    navigate("/Rightspage", { state: { data } });
   }
 
   return (
-    <div className='text-center flex' style={{"justifyContent":"center","flexDirection":"column"}} onClick={() => handleSubmit(imgSource, name, price)}>
+    <div
+      className="text-center flex"
+      style={{ justifyContent: "center", flexDirection: "column" }}
+      onClick={() => handleSubmit(imgSource, name, nftId)}
+    >
       <div className="h-47 w-44 border-4 border-black hover:border-4 hover:border-white">
-        <img src={imgSource} alt="select Nfts" style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'cover' }} />
+        <img
+          src={imgSource}
+          alt="select Nfts"
+          style={{ maxWidth: "100%", maxHeight: "100%", objectFit: "cover" }}
+        />
       </div>
-      <div className='my-1 flex max-w-[175px]' style={{"justifyContent":"center"}}>
-        <div className='text-blue-700 font-sans text-l font-semibold'>{name}</div>
+      <div
+        className="my-1 flex max-w-[175px]"
+        style={{ justifyContent: "center" }}
+      >
+        <div className="text-blue-700 font-sans text-l font-semibold">
+          {name}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
